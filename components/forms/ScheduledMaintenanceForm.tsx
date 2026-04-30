7377aa458b115b58c3698a2ddf6a85e2521ce6a6
aridosmarraque-droid <aridosmarraque@gmail.com>
2026-04-30 12:49:41 +0200

    Update ScheduledMaintenanceForm.tsx

--- components/forms/ScheduledMaintenanceForm.tsx
+++ components/forms/ScheduledMaintenanceForm.tsx
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect } from 'react';
 import { Machine, OperationLog, ServiceProvider, MaintenanceDefinition, Worker } from '../../types';
 import { getServiceProviders, getWorkers } from '../../services/db';
